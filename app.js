cc780649c7dee2757624cccfafb9bee8206ffd1d
“Tyler <tschoppe@umass.edu>
2020-01-15 17:43:34 -0500

    Annoying

--- app.js
+++ app.js
@@ -48,7 +48,7 @@ app.use(function(err, req, res, next) {
   res.render("error");
 });
 
-app.get("/home", (req, res) => {
+app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "client", "build", "index.html"));
 });
 
